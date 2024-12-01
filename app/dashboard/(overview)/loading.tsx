0e73528cf4d38c3c8438d084e5b422ab40c3c66f
codezyra <91514161+Sudila-Jayasekara@users.noreply.github.com>
2024-12-01 16:10:09 +0530

    create invoice

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,7 @@
 import DashboardSkeleton from "../../ui/skeletons";
 
+//if all dynamic compoenents in page wrapped with suspense, this loading compoenent will not show,
+//comment one suspense in page.tsx to see this loading component
 export default function Loading() {
-  return <DashboardSkeleton />;
+  return <p>Loading....</p>;
 }
